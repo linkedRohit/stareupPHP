847806c1541d1f316d083300d3e09644c75b7d8b
Rohit Sharma <rohit.sharma1@naukri.com>
2016-05-08 15:08:02 +0530

    Signup in progress

--- web/js/main.js
+++ web/js/main.js
@@ -91,6 +91,8 @@
 						off:		function() { $header.removeClass('alt'); }
 					});
 
+					$("#modal_trigger").leanModal({top : 100, overlay : 0.6, closeButton: ".modal_close" });
+					
 				});
 
 			}
