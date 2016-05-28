215d6c430844875c6584cf9be687d1e6a7f135d6
Rohit Sharma <rohit.sharma1@naukri.com>
2016-05-28 19:03:51 +0530

    Add the new routes actions and templates for seller and buyer profiles / actions

--- src/StareUp/Main/Bundle/Controller/BuyerController.php
+++ src/StareUp/Main/Bundle/Controller/BuyerController.php
@@ -3,11 +3,36 @@
 namespace StareUp\Main\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as config;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @config\Route("/buyer")
+ */
 class BuyerController extends Controller
 {
+     /**
+     * @config\Route("", name="buyer_profile")
+     * detail - This will return the buyer profile.
+     * author - Rohit Sharma
+     * date - 28 May 2016
+     */
     public function indexAction()
     {
         return $this->render('StareUpMainBundle:Buyer:index.html.twig');
     }
+
+    /**
+     * @config\Route("/post", name="post_asset")
+     * detail - action that helps you post assets for the rest of Startup World
+     * author - Rohit Sharma
+     * date - 28 May 2016
+     */
+    public function postAction()
+    {
+        return new Response('itemsold');
+    }
 }
+
--- src/StareUp/Main/Bundle/Controller/SellerController.php
+++ src/StareUp/Main/Bundle/Controller/SellerController.php
@@ -3,16 +3,35 @@
 namespace StareUp\Main\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as config;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @config\Route("/seller")
+ */
 class SellerController extends Controller
 {
+    /**
+     * @config\Route("", name="seller_profile")
+     * detail - This will return the seller profile
+     * author - Rohit Sharma
+     * date - 28 May 2016
+     */
     public function indexAction()
     {
         return $this->render('StareUpMainBundle:Seller:index.html.twig');
     }
 
+    /**
+     * @config\Route("/post", name="post_asset")
+     * detail - action that helps you post assets for the rest of Startup World
+     * author - Rohit Sharma
+     * date - 28 May 2016
+     */
     public function postAction()
     {
-	return 'itemsold';
+	return $this->render('StareUpMainBundle:Seller:post.html.twig');
     }
 }
